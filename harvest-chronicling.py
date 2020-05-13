eb9d50c1b92f9682e04a33492f4c4e84db6e6a0e
briancroxall <b.croxall@gmail.com>
2020-05-13 14:43:17 -0600

    Create indices for all pages

--- harvest-chronicling.py
+++ harvest-chronicling.py
@@ -10,5 +10,19 @@ https://chroniclingamerica.loc.gov/about/api/#search.
 """
 import os
 import requests as r
+from bs4 import BeautifulSoup 
 
+
+
+# Create list of all pages needed
+stem_url = 'https://chroniclingamerica.loc.gov/search/pages/results/?dateFilterType=range&date1=11%2F01%2F1890&date2=03%2F31%2F1891&format=atom&page='  # noqa E501
+pages = range(1, 3839)
+
+indices = []
+
+for each in pages:
+    indices.append(stem_url + str(each))
+
+
+test = 'https://chroniclingamerica.loc.gov/search/pages/results/?dateFilterType=range&date1=11%2F01%2F1890&date2=03%2F31%2F1891&format=atom&page1'
 
