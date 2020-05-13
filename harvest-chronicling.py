c1176987ec8841c063d369717ef234c2f25f5f2d
briancroxall <b.croxall@gmail.com>
2020-05-13 17:13:41 -0600

    Update data download to run across whole corpus

--- harvest-chronicling.py
+++ harvest-chronicling.py
@@ -12,6 +12,7 @@ import os
 import requests as r
 from bs4 import BeautifulSoup 
 import re
+import time
 
 
 def make_soup(xml):
@@ -22,17 +23,59 @@ def make_soup(xml):
 # Create directories
 if not os.path.isdir('data'):
     os.mkdir('data')
+    
+# Create output files
+with open('data-dictionary.tsv', 'w') as save_file:
+    pass
 
 # Create list of all pages needed
 stem_url = 'https://chroniclingamerica.loc.gov/search/pages/results/?dateFilterType=range&date1=11%2F01%2F1890&date2=03%2F31%2F1891&format=atom&page='  # noqa E501
 pages = range(1, 3839)
-
 indices = []
 
 for each in pages:
     indices.append(stem_url + str(each))
 
 
+# Download and save atom data
+print('Starting atom file download...')
+headers1 = {'user-agent': 'Brian Croxall (brian.croxall@byu.edu)'}  # noqa: E501 identify self
+for counter, each in enumerate(indices):
+    time.sleep(0.25)  # don't kill the server
+    if counter % 100 == 0:
+        print('.', end='', flush=True)
+    data = r.get(each, headers=headers1)
+    name = f'results-page-{counter + 1}'
+    with open(f'data/{name}', 'w') as new_file:
+        print(data.text, file=new_file)
+print('Atom files downloaded.\n')
+
+
+
+
+
+# Create output file for data
+with open('data-dictionary.tsv', 'w') as save_file:
+    pass
+
+# Process test data
+with open('data/test.xml') as data_file:
+    soup = make_soup(data_file)
+    entries = []
+    for each in soup.find_all('entry'):
+        entries.append(each)
+    for entry in entries:
+        link = entry.find('link').get('href')
+        date = link.split('/')[5]
+        img_num = link.split('/')[7]
+        title_tag = entry.find('title').get_text()
+        location = re.findall(r'\((.*?)\)', title_tag, flags=re.I)[0]  # https://regex101.com/r/e5WQw8/1
+        newspaper = re.findall(r'(.*?)\.\s', title_tag, flags=re.I)[0]  # https://regex101.com/r/e5WQw8/2
+        with open('data-dictionary.tsv', 'a') as save_file:
+            print(newspaper, date, img_num, link, sep='\t', file=save_file)
+
+
+"""
 test = 'https://chroniclingamerica.loc.gov/search/pages/results/?dateFilterType=range&date1=11%2F01%2F1890&date2=03%2F31%2F1891&format=atom&page1'  #noqa: E501
 
 # Download and save test data
@@ -63,3 +106,4 @@ with open('data/test.xml') as data_file:
         newspaper = re.findall(r'(.*?)\.\s', title_tag, flags=re.I)[0]  # https://regex101.com/r/e5WQw8/2
         with open('data-dictionary.tsv', 'a') as save_file:
             print(newspaper, date, img_num, link, sep='\t', file=save_file)
+"""
