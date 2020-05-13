e18340f1846db36901489b78d869649d1f1f2a1e
briancroxall <b.croxall@gmail.com>
2020-05-13 14:33:53 -0600

    Initial commit

---
+++ harvest-chronicling.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 14:26:28 2020
+
+@author: briancroxall
+
+Script to harvest targeted data from Chronicling America website,
+https://chroniclingamerica.loc.gov/about/api/#search.
+"""
+import os
+import requests as r
+
+
