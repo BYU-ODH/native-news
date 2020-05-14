c5cb3ac861a87828080ac81a36217cb5dd207a6f
briancroxall <b.croxall@gmail.com>
2020-05-14 14:01:41 -0600

    Create ocr-harvester.py

---
+++ ocr-harvester.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 14 14:00:51 2020
+
+@author: briancroxall
+
+This script reads data-dictionary.tsv and uses its content to harvest OCR for
+each newspaper page in our data set and saves it to the XXX directory.
+"""
+
