87f557df5896b172ab72e919342ebb0691c72780
briancroxall <b.croxall@gmail.com>
2020-05-14 13:21:22 -0600

    Add documentation

--- atom-reader.py
+++ atom-reader.py
@@ -4,5 +4,9 @@
 Created on Thu May 14 13:19:20 2020
 
 @author: briancroxall
+
+Script to manage the second portion of the data processing. This script will
+open each result from harvesting-chronicling.py and compile its data into one
+data dictionary.
 """
 
