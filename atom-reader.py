944c8e619dbc0ccd842b6beb3475ff3f951786c7
briancroxall <b.croxall@gmail.com>
2020-05-14 16:40:54 -0600

    Generalize process

--- atom-reader.py
+++ atom-reader.py
@@ -11,9 +11,16 @@ data dictionary.
 """
 from datetime import datetime
 from glob import glob
-
+from bs4 import BeautifulSoup
+import re
 
 startTime = datetime.now()
+
+
+def make_soup(xml):
+    soup = BeautifulSoup(xml, 'lxml-xml')
+    return soup
+
 
 # Create output files
 with open('data-dictionary.tsv', 'w') as save_file:
@@ -22,22 +29,25 @@ with open('data-dictionary.tsv', 'w') as save_file:
 # Corpus
 data = glob('atom-data/*.xml')
 
-# Process test data
-with open('data/test.xml') as data_file:
-    soup = make_soup(data_file)
-    entries = []
-    for each in soup.find_all('entry'):
-        entries.append(each)
-    for entry in entries:
-        link = entry.find('link').get('href')
-        ocr = link = 'ocr/'
-        date = link.split('/')[5]
-        img_num = link.split('/')[7]
-        title_tag = entry.find('title').get_text()
-        location = re.findall(r'\((.*?)\)', title_tag, flags=re.I)[0]  # https://regex101.com/r/e5WQw8/1
-        newspaper = re.findall(r'(.*?)\.\s', title_tag, flags=re.I)[0]  # https://regex101.com/r/e5WQw8/2
-        with open('data-dictionary.tsv', 'a') as save_file:
-            print(newspaper, date, img_num, link, ocr, sep='\t',
-                  file=save_file)
+# Process data
+for counter, each in enumerate(data):
+    if counter % 100 == 0:
+        print('.', end='', flush=True)
+    with open(each) as data_file:
+        soup = make_soup(data_file)
+        entries = []
+        for each in soup.find_all('entry'):
+            entries.append(each)
+        for entry in entries:
+            link = entry.find('link').get('href')
+            ocr = link + 'ocr/'
+            date = link.split('/')[5]
+            img_num = link.split('/')[7]
+            title_tag = entry.find('title').get_text()
+            location = re.findall(r'\((.*?)\)', title_tag, flags=re.I)[0]  # https://regex101.com/r/e5WQw8/1
+            newspaper = re.findall(r'(.*?)\.\s', title_tag, flags=re.I)[0]  # https://regex101.com/r/e5WQw8/2
+            with open('data-dictionary.tsv', 'a') as save_file:
+                print(newspaper, date, img_num, link, ocr, sep='\t',
+                      file=save_file)
 
 print('Time elapsed: ', datetime.now() - startTime)
