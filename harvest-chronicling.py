573fa82e73d0860522c969475b27fd1e29cc1f5e
briancroxall <b.croxall@gmail.com>
2020-05-14 13:22:37 -0600

    Remove data output creation

--- harvest-chronicling.py
+++ harvest-chronicling.py
@@ -31,10 +31,6 @@ def make_soup(xml):
 if not os.path.isdir('atom-data'):
     os.mkdir('atom-data')
 
-# Create output files
-with open('data-dictionary.tsv', 'w') as save_file:
-    pass
-
 # Create list of all pages needed
 stem_url = 'https://chroniclingamerica.loc.gov/search/pages/results/?dateFilterType=range&date1=11%2F01%2F1890&date2=03%2F31%2F1891&format=atom&page='  # noqa E501
 pages = range(1, 3839)
