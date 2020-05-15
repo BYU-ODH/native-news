89f78a70ebe6480ad8ca6e9608eeeabb3b131bd7
briancroxall <b.croxall@gmail.com>
2020-05-15 13:17:50 -0600

    Fix documentation

--- ocr-harvester.py
+++ ocr-harvester.py
@@ -6,7 +6,7 @@ Created on Thu May 14 14:00:51 2020
 @author: briancroxall
 
 This script reads data-dictionary.tsv and uses its content to harvest OCR for
-each newspaper page in our data set and saves it to the XXX directory.
+each newspaper page in our data set and saves it to the ocr-txt directory.
 """
 
 import requests as r
