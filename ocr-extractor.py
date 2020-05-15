9e298abcb1f20ce2a10552ec74758054042b07d7
briancroxall <b.croxall@gmail.com>
2020-05-15 12:45:00 -0600

    Initial commit

---
+++ ocr-extractor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 15 12:43:56 2020
+
+@author: briancroxall
+
+This script extracts the OCR text from the HTML from Chronicling America.
+"""
+
+from glob import glob
+from bs4 import BeautifulSoup
+
+
