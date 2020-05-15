59ead9ff1bbc8a354f0af25d81e5c2b6746b8690
briancroxall <b.croxall@gmail.com>
2020-05-15 12:47:11 -0600

    Create corpus

--- ocr-extractor.py
+++ ocr-extractor.py
@@ -12,3 +12,13 @@ from glob import glob
 from bs4 import BeautifulSoup
 
 
+def make_soup(xml):
+    soup = BeautifulSoup(xml, 'lxml-xml')
+    return soup
+
+corpus = sorted(glob('ocr-html/*.html'))
+test = ['ocr-html/the-republican-journal_1890-12-04_p2.html',
+        'ocr-html/st_1891-03-19_p8.html',
+        'ocr-html/the-state-republican_1891-02-26_p3.html']
+
+
