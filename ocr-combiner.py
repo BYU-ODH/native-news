977570394df8e919cba05441b2929cd324a4419a
briancroxall <b.croxall@gmail.com>
2020-05-18 17:22:54 -0600

    Initial commit

---
+++ ocr-combiner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 18 17:21:22 2020
+
+@author: briancroxall
+
+This script will combine the different pages of an issue of a newspaper into
+a single document.
+"""
+
+from glob import glob
+import os
