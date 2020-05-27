ab0b7a3701867d88ccdef9deb44adfd3b1d98a1b
briancroxall <b.croxall@gmail.com>
2020-05-27 09:52:03 -0600

    Add regex and save file. Finish for-loop.

--- search.py
+++ search.py
@@ -7,11 +7,22 @@ Created on Wed May 27 09:37:20 2020
 """
 
 from glob import glob
+import re
 
 
+# Corpus
 corpus = glob('combined-ocr/*.txt')
 test = ['combined-ocr/abilene-weekly-reflector_1890-11-06.txt',
         'combined-ocr/alexandria-gazette_1891-02-18.txt']
+
+# Output file
+with open('search-wounded.tsv', 'w') as output:
+    print('filename', 'newspaper', 'date', '# hits', 'strings', sep='\t',
+          file=output)
+
+
+# Regex
+re_wk = r'(w[0o]und[@e3]d kn[e@3][e@3])'
 
 
 for counter, each in enumerate(test):
@@ -20,5 +31,11 @@ for counter, each in enumerate(test):
     filename = each.split('/')[-1]
     no_ext = filename.split('.')[0]
     newspaper, date = no_ext.split('_')
+    with open(each) as in_file:
+        text = in_file.read()
+    results = re.findall(re_wk, text, flags=re.I)
+    with open('search-wounded.tsv', 'a') as save_file:
+        print(filename, newspaper, date, len(results), results, sep='\t',
+              file=save_file)
     
     
