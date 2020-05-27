582d8343913d92bfe0745c5ebc449e64b3bed0cb
briancroxall <b.croxall@gmail.com>
2020-05-27 09:59:38 -0600

    Sort corpus. Add counter for total number of mentions.

--- search.py
+++ search.py
@@ -11,7 +11,7 @@ import re
 
 
 # Corpus
-corpus = glob('combined-ocr/*.txt')
+corpus = sorted(glob('combined-ocr/*.txt'))
 test = ['combined-ocr/abilene-weekly-reflector_1890-11-06.txt',
         'combined-ocr/alexandria-gazette_1891-02-18.txt']
 
@@ -20,12 +20,13 @@ with open('search-wounded.tsv', 'w') as output:
     print('filename', 'newspaper', 'date', '# hits', 'strings', sep='\t',
           file=output)
 
-
 # Regex
 re_wk = r'(w[0o]und[@e3]d kn[e@3][e@3])'
 
+# Counters
+wk_counter = 0
 
-for counter, each in enumerate(test):
+for counter, each in enumerate(corpus):
     if counter % 100 == 0:
         print('.', end='', flush=True)
     filename = each.split('/')[-1]
@@ -34,6 +35,8 @@ for counter, each in enumerate(test):
     with open(each) as in_file:
         text = in_file.read()
     results = re.findall(re_wk, text, flags=re.I)
+    if len(results) >= 1:
+        wk_counter += 1
     with open('search-wounded.tsv', 'a') as save_file:
         print(filename, newspaper, date, len(results), results, sep='\t',
               file=save_file)
