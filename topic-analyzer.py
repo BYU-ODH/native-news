917375153aa68abaf5bc1b2adcd90b971b722666
briancroxall <b.croxall@gmail.com>
2020-05-28 10:43:32 -0600

    Save output

--- topic-analyzer.py
+++ topic-analyzer.py
@@ -23,8 +23,16 @@ def cast_date(date):
     return (year, month, day)
 
 
+with open('tm_wk_results.tsv', 'w') as output:
+    print('newspaper', 'date', 'wk', sep='\t', file=output)
+
 with open('topic-modeling/corp_stop_removed_100tops_comp.txt') as results:
     for line in results:
         indices = line.split('\t')
         newspaper, date = get_news_date(indices)
         year, month, day = cast_date(date)
+        wk_topic = float(indices[72])
+        if wk_topic < 0.001:
+            wk_topic = 0
+        with open('tm_wk_results.tsv', 'a') as output:
+            print(newspaper, date, round(wk_topic, 4), sep='\t', file=output)
