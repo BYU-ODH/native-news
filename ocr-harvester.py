6248722a543f0aaabe3532a194e232d7e1b4b24b
briancroxall <b.croxall@gmail.com>
2020-06-09 17:01:03 -0600

    Add location to newspapers set. Add locations set. Update filename to include locations.

--- ocr-harvester.py
+++ ocr-harvester.py
@@ -27,6 +27,8 @@ if not os.path.isdir('ocr-txt'):
 
 # Sets
 newspapers = set()
+locations = set()
+
 
 with open('data-dictionary.tsv') as data_file:
     for counter, line in enumerate(data_file):
@@ -36,17 +38,22 @@ with open('data-dictionary.tsv') as data_file:
         #     continue
         # else:
         newspaper, location, date, img_num, link, ocr_link = line.split('\t')
-        newspapers.add(newspaper)  # add newspaper title to set before I lower and kebab case it
-        newspaper = newspaper.lower().replace(' ', '-')
+        newsloc = newspaper + ' ' + location
+        newspapers.add(newsloc)  # add newspaper title and location to set before I lower and kebab case it
+        newspaper = newspaper.lower().replace(' ', '-').replace('.','')
+        location = location.lower().replace(' ', '-')
+        locations.add(location)
         img_num = img_num.replace('seq-', '')  # remove seq- from each page number
         time.sleep(0.25)
         ocr_data = r.get(ocr_link, headers=headers1)
-        filename = f'{newspaper}_{date}_p{img_num}.txt'
+        filename = f'{newspaper}_{location}_{date}_p{img_num}.txt'
         with open(f'ocr-txt/{filename}', 'w') as new_file:
             print(ocr_data.text, file=new_file)
         
 
 with open('newspapers.txt', 'w') as file:
     pprint(newspapers, stream=file)
+with open('locations.txt', 'w') as file:
+    pprint(locations, stream=file)
 
 print('\nTime elapsed: ', datetime.now() - startTime)
