cc718d80428d081b91750327f6026ea91a43ddc4
briancroxall <b.croxall@gmail.com>
2020-06-10 11:18:42 -0600

    Update code to include location in filenames.

--- ocr-combiner.py
+++ ocr-combiner.py
@@ -20,8 +20,9 @@ startTime = datetime.now()
 
 def get_filename(file):
     no_dir = file.split('/')[-1]
-    no_ext = no_dir.split('.')[0]
-    return no_ext
+    no_ext = no_dir.split('.')[:-1]
+    joined = '.'.join(no_ext)
+    return joined
 
 
 # Corpus
@@ -52,31 +53,33 @@ newsdict = defaultdict(list)
 
 
 # Build dictionary
-for each in test2:
+for counter, each in enumerate(corpus):
+    if counter % 100 == 0:
+        print('.', end='', flush=True)
     filename = get_filename(each)
     newspaper, location, date, page = filename.split('_')
     try:
-        newsdict[(newspaper, date)].append(page)
+        newsdict[(newspaper, location, date)].append(page)
     except KeyError:
-        newsdict[(newspaper, date)] = []
+        newsdict[(newspaper, location, date)] = []
         
-print('Dictionary built')
+print('\nDictionary built')
 print('Expected number of newspaper issues: ', len(newsdict))
 
 print('\nStarting to combine texts')
-for (newspaper, date), pages in newsdict.items():
+for (newspaper, location, date), pages in newsdict.items():
     for page in pages:
-        with open(f'ocr-txt/{newspaper}_{date}_{page}.txt') as ocr:
+        with open(f'ocr-txt/{newspaper}_{location}_{date}_{page}.txt') as ocr:
             text = ocr.read()
         if page == 'p1':
-            with open(f'combined-ocr/{newspaper}_{date}.txt', 'w') as save_file:
+            with open(f'combined-ocr/{newspaper}_{location}_{date}.txt', 'w') as save_file:
                 print(text, file=save_file)
         else:
-            with open(f'combined-ocr/{newspaper}_{date}.txt', 'a') as save_file:
+            with open(f'combined-ocr/{newspaper}_{location}_{date}.txt', 'a') as save_file:
                 print(text, file=save_file)
                 
 num_issues = len(glob('combined-ocr/*.txt'))
-print('\nTotal number of newspaper issues combined: ', num_issues)
+print('\nTotal number of combined newspaper issues: ', num_issues)
 if len(newsdict) == num_issues:
     print('Everything looks okay!')
 else: 
