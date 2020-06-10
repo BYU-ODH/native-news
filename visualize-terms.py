b03f192e31445f9b38bb119ad4c0ad9595e8874a
briancroxall <b.croxall@gmail.com>
2020-06-10 13:50:22 -0600

    Identify data columns

--- visualize-terms.py
+++ visualize-terms.py
@@ -9,10 +9,18 @@ Script to visualize search results from native news.
 """
 
 import os
+# from matplotlib import pyplot as plt
 
 if not os.path.isdir('images'):
     os.mkdir('images')
 
 with open('search-results.tsv') as file:
     for line in file:
+        filename = line.split('\t')[0]
+        newspaper = line.split('\t')[1]
+        location = line.split('\t')[2]
+        date = line.split('\t')[3]
+        wk_hits = line.split('\t')[4]
+        pr_hits = line.split('\t')[14]
+        gd_hits = line.split('\t')[22]
         
