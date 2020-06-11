ccb5b6b13f426ba5c49b56433d41a6f0249a3364
briancroxall <b.croxall@gmail.com>
2020-06-11 12:29:31 -0600

    Clean up code

--- visualize-terms.py
+++ visualize-terms.py
@@ -11,42 +11,31 @@ Script to visualize search results from native news.
 import os
 # from matplotlib import pyplot as plt
 from collections import defaultdict as dd
-# from collections import Counter
-# from pprint import pprint
     
-"""
-def find_week(day):
-    if day in range(1, 8):
-        return 'week1'
-    elif day in range(8, 15):
-        return 'week2'
-    elif day in range(15, 21):
-        return 'week3'
-    elif day in range(21, 29):
-        return 'week4'
-    else:
-        return 'week5'
-"""
 
+# Directories
 if not os.path.isdir('images'):
     os.mkdir('images')
 
-
-hits_dict = dd(dict)  # [date][term][count]
-newspaper_dict = dd(dict)  # [newspaper][date][term][count]
-
+# Lists
 dates = ['1890-11-01', '1890-11-08', '1890-11-15', '1890-11-22', '1890-11-29',
          '1890-12-06', '1890-12-13', '1890-12-20', '1890-12-27', '1891-01-03',
          '1891-01-10', '1891-01-17', '1891-01-24', '1891-01-31', '1891-02-07',
          '1891-02-14', '1891-02-21', '1891-02-28', '1891-03-07', '1891-03-14',
          '1891-03-21', '1891-03-28']
-
 terms = ['wk', 'pr', 'gd']
 
-
+# Sets
 newspapers = set()
 
-# create dates in dict in the right order
+# Dictionaries
+hits_dict = dd(dict)  # [date][term][count]
+newspaper_dict = dd(dict)  # [newspaper][date][term][count]
+terms_dict = {'wk': 'Wounded Knee',
+             'pr': 'Pine Ridge',
+             'gd': 'ghost dance'}
+
+# Create dates in dict in the right order. 
 for date in dates:  
     for term in terms:
         hits_dict[date][term] = 0
@@ -58,11 +47,8 @@ with open('search-results.tsv') as file:
             print('.', end='', flush=True)
         if counter == 0:  # skips the header in the TSV
             continue
-        # elif counter > 10:
-        #     continue
         else:
-        # filename = line.split('\t')[0]
-            # print(line)
+            # filename = line.split('\t')[0]
             newspaper = line.split('\t')[1]
             newspapers.add(newspaper)
             location = line.split('\t')[2]
@@ -70,18 +56,13 @@ with open('search-results.tsv') as file:
             for date1, date2 in zip(dates, dates[1:]):  # Rob's clever hack
                 if date1 <= date < date2:
                     week_date = date1
-            # year_s, month_s, day_s = date.split('-')
-            # day = int(day_s)
-            # week_s = find_week(day)
-            # new_date = f'{year_s}-{month_s}-{week_s}'
             wk_hits = int(line.split('\t')[4])
             pr_hits = int(line.split('\t')[14])
             gd_hits = int(line.split('\t')[22])
             hits_dict[week_date]['wk'] += wk_hits
             hits_dict[week_date]['pr'] += pr_hits
             hits_dict[week_date]['gd'] += gd_hits
-            # except KeyError:
-            #     hits_dict[week_date]['wk'] = wk_hits
+
 """            
             try:
                 newspaper_dict[newspaper][week_date]['wk'] = wk_hits
@@ -92,8 +73,3 @@ with open('search-results.tsv') as file:
             # hits_dict[newspaper][week_date]['pr'] = pr_hits
             # hits_dict[newspaper][week_date]['gd'] = gd_hits
                 
-
-
-# freqs_dates = Counter(dates)
-# with open('date_freqs.txt', 'w') as output:
-#     pprint(freqs_dates, stream=output)
