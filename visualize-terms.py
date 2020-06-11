3a05898495066ee729e53a26c9eae065df286728
briancroxall <b.croxall@gmail.com>
2020-06-11 12:33:02 -0600

    PEP8 formatting

--- visualize-terms.py
+++ visualize-terms.py
@@ -11,7 +11,7 @@ Script to visualize search results from native news.
 import os
 # from matplotlib import pyplot as plt
 from collections import defaultdict as dd
-    
+
 
 # Directories
 if not os.path.isdir('images'):
@@ -32,11 +32,11 @@ newspapers = set()
 hits_dict = dd(dict)  # [date][term][count]
 newspaper_dict = dd(dict)  # [newspaper][date][term][count]
 terms_dict = {'wk': 'Wounded Knee',
-             'pr': 'Pine Ridge',
-             'gd': 'ghost dance'}
+              'pr': 'Pine Ridge',
+              'gd': 'ghost dance'}
 
-# Create dates in dict in the right order. 
-for date in dates:  
+# Create dates in dict in the right order.
+for date in dates:
     for term in terms:
         hits_dict[date][term] = 0
 
@@ -63,7 +63,7 @@ with open('search-results.tsv') as file:
             hits_dict[week_date]['pr'] += pr_hits
             hits_dict[week_date]['gd'] += gd_hits
 
-"""            
+"""
             try:
                 newspaper_dict[newspaper][week_date]['wk'] = wk_hits
             except KeyError:
@@ -72,4 +72,3 @@ with open('search-results.tsv') as file:
 """
             # hits_dict[newspaper][week_date]['pr'] = pr_hits
             # hits_dict[newspaper][week_date]['gd'] = gd_hits
-                
