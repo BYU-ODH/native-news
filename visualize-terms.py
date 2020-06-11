4cd7d24a06e7c27b77750d8bdfde397c68e4fa05
briancroxall <b.croxall@gmail.com>
2020-06-11 17:35:37 -0600

    Add loop for graphs. Add dictionary for numbered weeks.

--- visualize-terms.py
+++ visualize-terms.py
@@ -34,12 +34,38 @@ newspaper_dict = dd(dict)  # [newspaper][date][term][count]
 terms_dict = {'wk': 'Wounded Knee',
               'pr': 'Pine Ridge',
               'gd': 'ghost dance'}
+week_dict = {'1890-11-01': '1', 
+             '1890-11-08': '2',
+             '1890-11-15': '3',
+             '1890-11-22': '4',
+             '1890-11-29': '5',
+             '1890-12-06': '6',
+             '1890-12-13': '7',
+             '1890-12-20': '8',
+             '1890-12-27': '9',
+             '1891-01-03': '10',
+             '1891-01-10': '11',
+             '1891-01-17': '12',
+             '1891-01-24': '13',
+             '1891-01-31': '14',
+             '1891-02-07': '15',
+             '1891-02-14': '16',
+             '1891-02-21': '17',
+             '1891-02-28': '18',
+             '1891-03-07': '19',
+             '1891-03-14': '20',
+             '1891-03-21': '21',
+             '1891-03-28': '22'}
 
 # Create dates in dict in the right order.
 for term in terms:
     for date in dates:
         hits_dict[term][date] = 0
-
+    """
+    use this if I want to number weeks rather than have their full dates
+    for each in range(1,23):
+        hits_dict[term][each] = 0
+    """
 
 with open('search-results.tsv') as file:
     for counter, line in enumerate(file):
@@ -56,6 +82,7 @@ with open('search-results.tsv') as file:
             for date1, date2 in zip(dates, dates[1:]):  # Rob's clever hack
                 if date1 <= date < date2:
                     week_date = date1
+                    # week_date = int(week_dict[week_date])  # use this if I want to number weeks rather than have their full dates
             wk_hits = int(line.split('\t')[4])
             pr_hits = int(line.split('\t')[14])
             gd_hits = int(line.split('\t')[22])
@@ -63,8 +90,25 @@ with open('search-results.tsv') as file:
             hits_dict['pr'][week_date] += pr_hits
             hits_dict['gd'][week_date] += gd_hits
 
+for term in terms:
+    term_counts = hits_dict[term].items()
+    # for year, counts in term_counts:
+    plt.figure(figsize=(20,12))  # set the size of the graph
+    plt.plot(*zip(*term_counts), linewidth=4)  # get the data from the dictionary
+    ylim = plt.ylim()  # find the min and max of y on the graph
+    vert_y = ylim  # use the min and max as a range for the vertical line
+    vert_x = (8,8)  # place the vertical line where I want it. 0-indexed
+    plt.plot(vert_x, vert_y, linestyle=':', linewidth=2)  # draw the dashed, vertical line
+    plt.xticks(rotation='vertical')  # make the tick labels on the x axis rotate
+    plt.xlabel('Weeks', fontweight='bold')
+    plt.ylabel('Counts', fontweight='bold')
+    plt.suptitle(f'Total appearances of \'{terms_dict[term]}\'', fontsize=24)
+    plt.title('from 1 Nov 1890 - 31 Mar 1891', fontsize=16)
+    plt.savefig(f'images/week_count_{term}.png')
+    plt.clf()
 
-"""
+    
+""" 
             try:
                 newspaper_dict[newspaper][week_date]['wk'] = wk_hits
             except KeyError:
