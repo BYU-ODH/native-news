597eebc7748a1e1de5f003c94ae0c245dfcbdd5e
briancroxall <b.croxall@gmail.com>
2020-06-25 10:32:42 -0600

    Create map-terms.py

---
+++ map-terms.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 25 10:32:11 2020
+
+@author: briancroxall
+
+Script to compile the information we need for mapping appearances of terms.
+"""
+
